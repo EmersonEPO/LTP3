1e113db0a03c2de5987492b1d37a5cc0919a8481
aluno <aluno@aluno-desktop>
2013-03-21 08:35:27 -0300

    construção padrão de classe

---
+++ aula190313/src/br/edu/ifnmg/tads/lp3/Telefone.java
@@ -0,0 +1,30 @@
+package br.edu.ifnmg.tads.lp3;
+
+public class Telefone {
+    private int id;
+    private int numero;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Telefones{" + "id=" + id + ", numero=" + numero + '}';
+    }
+    
+    
+    
+}
