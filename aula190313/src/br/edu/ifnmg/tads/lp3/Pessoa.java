7af4e02569c235ad53d68cd9fd8a040e74b5a4bb
aluno <aluno@aluno-desktop>
2013-03-21 08:59:49 -0300

    construção padrão de classe

--- aula190313/src/br/edu/ifnmg/tads/lp3/Pessoa.java
+++ aula190313/src/br/edu/ifnmg/tads/lp3/Pessoa.java
@@ -1,16 +1,25 @@
 package br.edu.ifnmg.tads.lp3;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 
 public class Pessoa{
     
     private int id;
     private String nome;
+    private List<Email> emails;
+    private List<Telefone> telefones;
+    private List<Endereco> enderecos;
  
     
     public Pessoa(){
-        this.id=0;
-        this.nome="vazio";
+        id=0;
+        nome="vazio";
+        emails= new LinkedList<Email>();
+        telefones= new LinkedList<Telefone>();
+        enderecos= new LinkedList<Endereco>();
+        
     }
     
     public int getId() {
@@ -34,6 +43,42 @@ public class Pessoa{
         throw new Exception("Não podem haver nomes com menos de 3 letras e mais que 250 letras");
     }
 
+    public List<Email> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(List<Email> emails) {
+        this.emails = emails;
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Telefone> telefones) {
+        this.telefones = telefones;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public void addEmail(Email obj){
+        if(!emails.contains(obj)){
+            emails.add(obj);
+        }
+    }
+    
+    public void removeEmail(Email obj){
+        if(emails.contains(obj)){
+            emails.remove(obj);
+        }
+    }
+            
     @Override
     public int hashCode() {
         int hash = 3;
